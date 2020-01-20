0c42fc5293ec76b7aebd81c5a2050133068b8746
Alex Olivera <oalex90@gmail.com>
2020-01-20 15:15:09 -0600

    Update logo
    Flattened logo image so that it is one layer

--- dist/shorturl.afb73442.js
+++ dist/shorturl.afb73442.js
@@ -11059,7 +11059,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56297" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57961" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
