3ddddd591b599ba2f0c721b9311566f097f24476
Alex Olivera <oalex90@gmail.com>
2020-02-04 17:17:41 -0600

    Made roguelike mobile-friendly
    -shifted to a mobile-first design
    -resized board based on screen width
    -added mobile buttons to move player
    -slight redesign of page

--- dist/scatterplot.855179eb.js
+++ dist/scatterplot.855179eb.js
@@ -39810,7 +39810,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61539" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51131" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
