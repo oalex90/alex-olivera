c6d1430def10d0c9d7c9e8be419c8dc5545fd015
Alex Olivera <oalex90@gmail.com>
2020-02-25 10:42:48 -0600

    packaging issue fix

--- dist/chatroom.7f5173ca.js
+++ dist/chatroom.7f5173ca.js
@@ -20370,7 +20370,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54748" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64829" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
